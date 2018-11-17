83ef83892b9bbe916f374ceef73b69b52789325c
David Shvimer <david@Davids-MacBook-Pro.local>
2018-11-17 16:03:47 -0500

    Removed logic

--- app/models/post.rb
+++
@@ -1,6 +0,0 @@
-class Post < ApplicationRecord
-  belongs_to :user
-
-  validates :body,
-            presence: true
-end
--- app/serializers/comment_serializer.rb
+++
@@ -1,5 +0,0 @@
-class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :body
-
-  belongs_to :user, serializer: UserSerializer
-end
